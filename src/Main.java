f9ac703ab859ba638f1bbf2c35df0284dea7b417
Maria Grebenkova <grebenkova.m.p@gmail.com>
2020-10-08 02:49:08 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_balance;
+        int transaction_sum;
+        int total;
+         current_balance = 2_000_000_000;
+         transaction_sum = 500_000_000;
+         total = current_balance + transaction_sum;
+                 System.out.println(total);
+
+    }
+}
